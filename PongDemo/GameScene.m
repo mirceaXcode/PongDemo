32f8e6c2bd6a7fdc3f89fd594695184fbac74335
Mircea Popescu <mirceap@gmail.com>
2018-10-10 12:39:13 -0500

    Basic Pong Game

--- PongDemo/GameScene.m
+++ PongDemo/GameScene.m
@@ -8,10 +8,17 @@
 
 #import "GameScene.h"
 
-@implementation GameScene {
-    SKShapeNode *_spinnyNode;
-    SKLabelNode *_label;
-}
+
+@interface GameScene()
+
+@property (strong, nonatomic) UITouch *leftPaddleMotivatingTouch;
+@property (strong, nonatomic) UITouch *rightPaddleMotivatingTouch;
+
+@end
+
+@implementation GameScene
+
+static const CGFloat kTrackPixelsPerSecond = 500;
 
 - (void)didMoveToView:(SKView *)view {
     // Setup your scene here
@@ -25,46 +32,71 @@
 }
 
 
-- (void)touchDownAtPoint:(CGPoint)pos {
-    SKShapeNode *n = [_spinnyNode copy];
-    n.position = pos;
-    n.strokeColor = [SKColor greenColor];
-    [self addChild:n];
-}
-
-- (void)touchMovedToPoint:(CGPoint)pos {
-    SKShapeNode *n = [_spinnyNode copy];
-    n.position = pos;
-    n.strokeColor = [SKColor blueColor];
-    [self addChild:n];
-}
-
-- (void)touchUpAtPoint:(CGPoint)pos {
-    SKShapeNode *n = [_spinnyNode copy];
-    n.position = pos;
-    n.strokeColor = [SKColor redColor];
-    [self addChild:n];
-}
-
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    // Run 'Pulse' action from 'Actions.sks'
-    //[_label runAction:[SKAction actionNamed:@"Pulse"] withKey:@"fadeInOut"];
-    
-    for (UITouch *t in touches) {[self touchDownAtPoint:[t locationInNode:self]];}
+    for (UITouch *t in touches) {
+        CGPoint p = [t locationInNode:self];
+        NSLog(@"\n%f %f %f %f", p.x, p.y, self.frame.size.width, self.frame.size.height);
+
+        if(p.x < self.frame.size.width * 0.01){
+            self.leftPaddleMotivatingTouch = t;
+            NSLog(@"left");
+        } else if(p.x > self.frame.size.width * 0.1){
+            self.rightPaddleMotivatingTouch = t;
+            NSLog(@"right");
+        }
+        else {
+            // if ball gets stuck, we increase the velocity by clicking in the middle where norither left or right touch is registered and double it's velocity
+            SKNode *ball = [ self childNodeWithName:@"ball"];
+            ball.physicsBody.velocity = CGVectorMake(ball.physicsBody.velocity.dx*2.0, ball.physicsBody.velocity.dy);
+        }
+    }
+    [self trackPaddlesToMotivatingTouches];
 }
+
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
-    for (UITouch *t in touches) {[self touchMovedToPoint:[t locationInNode:self]];}
+   
+    [self trackPaddlesToMotivatingTouches];
+    
 }
+
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    for (UITouch *t in touches) {[self touchUpAtPoint:[t locationInNode:self]];}
+   
+    if([touches containsObject:self.leftPaddleMotivatingTouch])
+        self.leftPaddleMotivatingTouch = nil;
+    
+    if([touches containsObject:self.rightPaddleMotivatingTouch])
+        self.rightPaddleMotivatingTouch = nil;
 }
+
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
-    for (UITouch *t in touches) {[self touchUpAtPoint:[t locationInNode:self]];}
+   
+    if([touches containsObject:self.leftPaddleMotivatingTouch])
+        self.leftPaddleMotivatingTouch = nil;
+    
+    if([touches containsObject:self.rightPaddleMotivatingTouch])
+        self.rightPaddleMotivatingTouch = nil;
+    
 }
 
 
--(void)update:(CFTimeInterval)currentTime {
-    // Called before each frame is rendered
+-(void) trackPaddlesToMotivatingTouches {
+    
+    id a = @[@{@"node": [self childNodeWithName:@"left_paddle"],
+               @"touch": self.leftPaddleMotivatingTouch ?: [NSNull null]},
+             @{@"node": [self childNodeWithName:@"right_paddle"],
+               @"touch": self.rightPaddleMotivatingTouch ?: [NSNull null]}];
+    
+    for(NSDictionary *o in a){
+        SKNode *node = o[@"node"];
+        UITouch *touch = o[@"touch"];
+        if([[NSNull null] isEqual:touch])
+            continue;
+        CGFloat yPos = [touch locationInNode:self].y;
+        NSTimeInterval duration = ABS(yPos -node.position.y) / kTrackPixelsPerSecond;
+        
+        SKAction *moveAction = [SKAction moveToY:yPos duration:duration];
+        [node runAction:moveAction withKey:@"moving!"];
+    }
 }
 
 @end
